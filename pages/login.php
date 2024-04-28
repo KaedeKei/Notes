610115eb62385798075571645150fa1adc39db12
Kaede Kei <99081219+KaedeKei@users.noreply.github.com>
2024-04-29 02:30:56 +0300

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -1,5 +1,4 @@
 <?php
-//если есть активный пользователь - выводим приветствие и кнопку выхода
 if (isset($_SESSION["user"])) {
     echo "<form action='index.php";
     if (isset($_GET["page"])) echo "?page=" . $_GET['page'] . "'";
@@ -10,19 +9,15 @@ if (isset($_SESSION["user"])) {
     echo "<input type='submit' value='Выйти' id='exit' name='exit' class='btn btn-danger' />";
     echo "</div></form>";
 
-    //если нажата кнопка Выйти
     if (isset($_POST["exit"])) {
-        //сбрасываем пользователя в сессии
         unset($_SESSION["user"]);
         //unset($_SESSION["admin"]);
-        echo "<script>window.location.reload()</script>"; //перезагрузка страницы
+        echo "<script>window.location.reload()</script>"; 
     }
 } else {
-    //если в сессии нет активного пользователя - выводим форму аутентификации
     if (isset($_POST["enter"])) {
-        //если нажата кнопка Войти - перезагружаем страницу, чтобы сохранить активного пользователя и вывести приветствие
         if (login($_POST["login"], $_POST["pass"])) {
-            echo "<script>window.location.reload()</script>"; //перезагрузка страницы
+            echo "<script>window.location.reload()</script>"; 
         }
     } else {
         echo "<form action='index.php";
